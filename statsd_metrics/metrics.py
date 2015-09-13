46bf37cfbca1b97bf34e10218e74defc5147e3cf
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-09-13 15:32:09 +0800

    Add name for metrics, abstract metric as parent
    Renamed 'balance' property of Counter to 'count'.

--- statsd_metrics/metrics.py
+++ statsd_metrics/metrics.py
@@ -4,34 +4,54 @@ statsd_metrics.metrics
 Define metrics classes
 """
 
-class Counter(object):
-    def __init__(self, balance=0, sample_rate=1):
-        self._set_balance(balance)
-        self._set_sample_rate(sample_rate)
+from types import StringTypes, IntType, FloatType
+
+class AbstractMetric(object):
+    def __init__(self, name):
+        self._set_name(name)
 
     @property
-    def balance(self):
-        return self.__balance
+    def name(self):
+        return self._name
 
-    @balance.setter
-    def balance(self, balance):
-        self._set_balance(balance)
+    @name.setter
+    def name(self, name):
+        self._set_name(name)
+
+    def _set_name(self, name):
+        assert type(name) in StringTypes, 'Metric name should be string'
+        assert name != '', 'Metric name should not be empty'
+        self._name = name
+
+class Counter(AbstractMetric):
+    def __init__(self, name, count=0, sample_rate=1.0):
+        super(Counter, self).__init__(name)
+        self.count = count
+        self.sample_rate = sample_rate
+
+    @property
+    def count(self):
+        return self._count
+
+    @count.setter
+    def count(self, count):
+        assert type(count) is IntType, 'Counter count should be integer'
+        self._count = count
 
     @property
     def sample_rate(self):
-        return self.__sample_rate
+        return self._sample_rate
 
     @sample_rate.setter
     def sample_rate(self, value):
-        self._set_sample_rate(value)
+        assert type(value) is FloatType, 'Counter sample rate should be float'
+        assert value > 0, 'Counter sample rate should be positive'
+        self._sample_rate = value
 
-    def _set_balance(self, balance):
-        assert balance == int(balance)
-        self.__balance = balance
-
-    def _set_sample_rate(self, rate):
-        rate_float = float(rate)
-        assert rate_float > 0
-        self.__sample_rate = float(rate_float)
+    def to_request(self):
+        result = "{0}:{1}|c".format(self._name, self._count)
+        if self._sample_rate != 1.0:
+            result += "@{:.1}".format(self._sample_rate)
+        return result
 
 __all__ = (Counter,)
