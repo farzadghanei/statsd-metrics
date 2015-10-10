3550c43eef5ca54e7fa2a4cc7df077b6f3d111b7
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-10-10 16:57:38 +0800

    Set version to 0.3.0

--- statsdmetrics/__init__.py
+++ statsdmetrics/__init__.py
@@ -13,7 +13,7 @@ from .metrics import (Counter, Timer, Gauge,
                       parse_metric_from_request,
                     )
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 __all__ = (Counter, Timer, Gauge,
            Set, GaugeDelta,
