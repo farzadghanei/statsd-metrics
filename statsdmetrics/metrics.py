ca86dbd0eaf295440adad8a80861f433a471b767
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-10-29 12:06:39 +0200

    Add typin as a requirement for development, add type hints to metrics

--- statsdmetrics/metrics.py
+++ statsdmetrics/metrics.py
@@ -6,24 +6,33 @@ Define metric classes
 
 from abc import ABCMeta, abstractmethod
 from re import compile, sub
+try:
+    from typing import Any, Dict, Tuple, Union
+    TypeMetric = Union['AbstractMetric', 'Counter', 'Timer', 'Gauge', 'GaugeDelta', 'Set']
+except ImportError:
+    Any, Dict, Tuple, Union = None, None, None, None, None  # type: ignore
+    TypeMetric = None
+
 
 try:
-    unicode('')
+    unicode('')  # type: ignore
 except NameError:
     unicode = str
 
 try:
-    long(1)
+    long(1)  # type: ignore
 except NameError:
     long = int
 
 
 def is_string(value):
+    # type: (Any) -> bool
     return isinstance(value, str) or \
            isinstance(value, unicode)
 
 
 def is_numeric(value):
+    # type: (Any) -> bool
     return isinstance(value, int) or \
            isinstance(value, float) or \
            isinstance(value, long)
@@ -33,33 +42,27 @@ normalize_metric_name_regex_subs = (
     (compile("\s+"), "_"),
     (compile("[\/\\\\]"), "-"),
     (compile("[^\w.-]"), ""),
-)
+)  # type: Tuple[Tuple[Any, str], Tuple[Any, str], Tuple[Any, str]]
 
 
 def normalize_metric_name(name):
+    # type: (unicode) -> unicode
     for regex, replacement in normalize_metric_name_regex_subs:
         name = sub(regex, replacement, name)
     return name
 
 
 def parse_metric_from_request(request):
+    # type: (unicode) -> TypeMetric
     assert is_string(request), \
         "Request should be string to parse a metric from"
-    metric_type_classes = dict(
-        c=Counter,
-        ms=Timer,
-        g=Gauge,
-        s=Set,
-    )
-    metric_value_types = dict(
-        c=int,
-        ms=float,
-        g=float,
-    )
 
-    name, data = request.split(':')
-    value, _, type_section = data.partition('|')
-    type_, _, sample_rate_section = type_section.partition('|@')
+    metric_type_classes = {'c': Counter, 'ms': Timer, 'g': Gauge, 's': Set}
+    metric_value_types = {'c': int, 'ms': float, 'g': float}
+
+    name, data = request.split(':')  # type: unicode, unicode
+    value, _, type_section = data.partition('|')  # type: unicode, unicode, unicode
+    type_, __, sample_rate_section = type_section.partition('|@')  # type: unicode, unicode, unicode
 
     if type_ not in metric_type_classes:
         raise ValueError(
@@ -69,13 +72,13 @@ def parse_metric_from_request(request):
     if type_ == 'g' and len(value) > 1 and value[0] in ('+', '-'):
         metric_class = GaugeDelta
     else:
-        metric_class = metric_type_classes[type_]
+        metric_class = metric_type_classes[type_]  # type: ignore
 
     value = metric_value_types[type_](value) \
-        if type_ in metric_value_types else value
+        if type_ in metric_value_types else value  # type: ignore
 
     sample_rate = AbstractMetric.default_sample_rate \
-        if sample_rate_section == '' else float(sample_rate_section)
+        if sample_rate_section == '' else float(sample_rate_section)  # type: float
 
     return metric_class(name.strip(), value, sample_rate)
 
@@ -83,11 +86,11 @@ def parse_metric_from_request(request):
 class AbstractMetric(object):
     __metaclass__ = ABCMeta
 
-    default_sample_rate = 1
+    default_sample_rate = 1  # type: float
 
-    def __init__(self, name):
-        self._name = ''
-        self._sample_rate = self.__class__.default_sample_rate
+    def __init__(self, name):  # type: (str) -> None
+        self._name = ''  # type: unicode
+        self._sample_rate = self.__class__.default_sample_rate  # type: ignore
         self.name = name
 
     @property
@@ -96,11 +99,12 @@ class AbstractMetric(object):
 
     @name.setter
     def name(self, name):
+        # type: (unicode) -> None
         assert is_string(name), \
             'Metric name should be string'
         assert name != '', \
             'Metric name should not be empty'
-        self._name = name
+        self._name = unicode(name)
 
     @property
     def sample_rate(self):
@@ -108,6 +112,7 @@ class AbstractMetric(object):
 
     @sample_rate.setter
     def sample_rate(self, value):
+        # type: (float) -> None
         assert is_numeric(value), \
             'Metric sample rate should be numeric: {}:{}'.format(
                 self.name, value)
@@ -117,14 +122,14 @@ class AbstractMetric(object):
         self._sample_rate = value
 
     @abstractmethod
-    def to_request(self):
+    def to_request(self):  # type: () -> bytes
         raise NotImplementedError()
 
 
 class Counter(AbstractMetric):
     def __init__(self, name, count=0, sample_rate=1):
         super(Counter, self).__init__(name)
-        self._count = 0
+        self._count = 0   # type: int
         self.count = count
         self.sample_rate = sample_rate
 
@@ -134,11 +139,13 @@ class Counter(AbstractMetric):
 
     @count.setter
     def count(self, count):
+        # type: (int) -> None
         assert isinstance(count, int), \
             'Counter count should be integer'
         self._count = count
 
     def to_request(self):
+        # type: () -> bytes
         result = "{0}:{1}|c".format(self._name, self._count)
         if self._sample_rate != 1:
             result += "|@{:n}".format(self._sample_rate)
@@ -162,7 +169,7 @@ class Counter(AbstractMetric):
 class Timer(AbstractMetric):
     def __init__(self, name, milliseconds, sample_rate=1):
         super(Timer, self).__init__(name)
-        self._milliseconds = 0
+        self._milliseconds = 0  # type: int
         self.milliseconds = milliseconds
         self.sample_rate = sample_rate
 
@@ -172,6 +179,7 @@ class Timer(AbstractMetric):
 
     @milliseconds.setter
     def milliseconds(self, milliseconds):
+        # type: (int) -> None
         assert is_numeric(milliseconds), \
             'Timer milliseconds should be numeric'
         assert milliseconds >= 0, \
@@ -179,6 +187,7 @@ class Timer(AbstractMetric):
         self._milliseconds = milliseconds
 
     def to_request(self):
+        # type: () -> bytes
         result = "{0}:{1}|ms".format(self._name, self._milliseconds)
         if self._sample_rate != 1:
             result += "|@{:n}".format(self._sample_rate)
@@ -201,7 +210,7 @@ class Timer(AbstractMetric):
 
 class Gauge(AbstractMetric):
     def __init__(self, name, value, sample_rate=1):
-        self._value = 0
+        self._value = 0  # type: float
         super(Gauge, self).__init__(name)
         self.value = value
         self.sample_rate = sample_rate
@@ -212,6 +221,7 @@ class Gauge(AbstractMetric):
 
     @value.setter
     def value(self, value):
+        # type: (float) -> None
         assert is_numeric(value), \
             'Gauge value should be numeric'
         assert value >= 0, \
@@ -219,6 +229,7 @@ class Gauge(AbstractMetric):
         self._value = value
 
     def to_request(self):
+        # type: () -> bytes
         result = "{0}:{1}|g".format(self._name, self._value)
         if self._sample_rate != 1:
             result += "|@{:n}".format(self._sample_rate)
@@ -241,7 +252,7 @@ class Gauge(AbstractMetric):
 
 class Set(AbstractMetric):
     def __init__(self, name, value, sample_rate=1):
-        self._value = 0
+        self._value = 0  # type: Any
         super(Set, self).__init__(name)
         self.value = value
         self.sample_rate = sample_rate
@@ -252,6 +263,7 @@ class Set(AbstractMetric):
 
     @value.setter
     def value(self, value):
+        # type: (Any) -> None
         try:
             str(value)
         except (TypeError, ValueError):
@@ -265,6 +277,7 @@ class Set(AbstractMetric):
         self._value = value
 
     def to_request(self):
+        # type: () -> bytes
         result = "{0}:{1}|s".format(self._name, self._value)
         if self._sample_rate != 1:
             result += "|@{:n}".format(self._sample_rate)
@@ -287,7 +300,7 @@ class Set(AbstractMetric):
 
 class GaugeDelta(AbstractMetric):
     def __init__(self, name, delta, sample_rate=1):
-        self._delta = 0
+        self._delta = 0  # type: float
         super(GaugeDelta, self).__init__(name)
         self.delta = delta
         self.sample_rate = sample_rate
@@ -298,11 +311,13 @@ class GaugeDelta(AbstractMetric):
 
     @delta.setter
     def delta(self, delta):
+        # type: (float) -> None
         assert is_numeric(delta), \
             "Gauge delta should be numeric"
         self._delta = delta
 
     def to_request(self):
+        # type: () -> bytes
         result = "{}:{:+n}|g".format(self._name, self._delta)
         if self._sample_rate != 1:
             result += "|@{:n}".format(self._sample_rate)
@@ -323,8 +338,8 @@ class GaugeDelta(AbstractMetric):
                or self.sample_rate != other.sample_rate
 
 
-__all__ = (Counter, Timer, Gauge,
-           Set, GaugeDelta,
-           normalize_metric_name,
-           parse_metric_from_request
-           )
+__all__ = ['Counter', 'Timer', 'Gauge',
+           'Set', 'GaugeDelta',
+           'normalize_metric_name',
+           'parse_metric_from_request'
+           ]
