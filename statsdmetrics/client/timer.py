13539b000babc65285863a97e2c9ab1dfc3801f6
Farzad Ghanei <farzad.ghanei@gmail.com>
2016-11-08 21:44:57 +0100

    Add client.timer.StopWatch class and allow setting rates for Timer
    So the a Timer may have a default sample rate (instead of always using 1
    as default), but each method that sends metrics may override the default
    sample rate.
    
    Assert valid values for sample rates.
    
    Add StopWatch to convinently send timing metrics from a given starting
    reference. This will help users to send metrics relative to a given poin
    in time. This also will be used as the context manager for timing calls.

--- statsdmetrics/client/timer.py
+++ statsdmetrics/client/timer.py
@@ -4,6 +4,7 @@ statsdmetrics.client.timer
 Easy to use classes to send timing metrics
 """
 import functools
+from datetime import datetime
 from time import time
 
 from statsdmetrics.client import AbstractClient
@@ -12,6 +13,17 @@ try:
     from typing import Union, Callable, Any
 except ImportError:
     Union, Callable, Any = None, None, None  # type: ignore
+
+
+def assert_timestamp(timestamp):
+    # type: (Any) -> None
+    assert isinstance(timestamp, int) or isinstance(timestamp, float)
+
+
+def assert_sample_rate(rate):
+    # type: (Any) -> None
+    assert isinstance(rate, int) or isinstance(rate, float)
+    assert 0 <= rate <= 1
 
 
 class ClientWrapper(object):
@@ -32,23 +44,59 @@ class ClientWrapper(object):
         self._client = client
 
 
-class Timer(ClientWrapper):
-    def since(self, name, timestamp, rate=1):
+class SampleRateMixIn(object):
+    def __init__(self, rate=1):
+        # type: (float) -> None
+        assert_sample_rate(rate)
+        self._rate = rate
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, rate):
+        # type: (float) -> None
+        assert_sample_rate(rate)
+        self._rate = rate
+
+
+class Timer(ClientWrapper, SampleRateMixIn):
+    def __init__(self, client, rate=1):
+        # type: (AbstractClient, float) -> None
+        SampleRateMixIn.__init__(self, rate)
+        ClientWrapper.__init__(self, client)
+
+    def since(self, name, timestamp, rate=None):
         # type (str, Union[float, datetime], float) -> Timer
+        if rate is None:
+            rate = self._rate
+        else:
+            assert_sample_rate(rate)
+
         self._client.timing_since(name, timestamp, rate)
         return self
 
-    def time_callable(self, name, target, rate=1, *args, **kwargs):
+    def time_callable(self, name, target, rate=None, *args, **kwargs):
         # type: (str, Callable, float, *Any, **Any) -> Timer
         """Send a Timer metric calculating duration of execution of the provided callable"""
         assert callable(target)
+        if rate is None:
+            rate = self._rate
+        else:
+            assert_sample_rate(rate)
         start_time = time()  # type: float
         result = target(*args, **kwargs)
         self.since(name, start_time, rate)
         return result
 
-    def wrap(self, name, rate=1):
+    def wrap(self, name, rate=None):
         # type: (str, float) -> Callable
+        if rate is None:
+            rate = self._rate
+        else:
+            assert_sample_rate(rate)
+
         def create_decorator(func):
             # type: (Callable) -> Callable
             @functools.wraps(func)
@@ -57,3 +105,44 @@ class Timer(ClientWrapper):
             return decorator
         return create_decorator
 
+
+class StopWatch(ClientWrapper, SampleRateMixIn):
+    def __init__(self, client, name, rate=1, reference=None):
+        # type: (AbstractClient, str, float, float) -> None
+        if reference is None:
+            reference = time()
+        else:
+            assert_timestamp(reference)
+        SampleRateMixIn.__init__(self, rate)
+        ClientWrapper.__init__(self, client)
+        self._name = str(name)
+        self._paused_duration = 0
+        self._reference = reference
+
+    @property
+    def reference(self):
+        return self._reference
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        # type: (str) -> None
+        self._name = str(name)
+
+    def reset(self):
+        # type: () -> StopWatch
+        """Reset stop watch by setting now as reference"""
+        self._reference = time()
+        return self
+
+    def send(self, rate=None):
+        # type: (float) -> StopWatch
+        if rate is None:
+            rate = self._rate
+        else:
+            assert_sample_rate(rate)
+        self.client.timing_since(self._name, self._reference, rate)
+        return self
--- tests/test_client_timer.py
+++ tests/test_client_timer.py
@@ -5,7 +5,7 @@ unit tests for timer module
 """
 from time import time, sleep
 from statsdmetrics.client import Client
-from statsdmetrics.client.timer import Timer
+from statsdmetrics.client.timer import Timer, StopWatch
 
 try:
     import unittest.mock as mock
@@ -22,6 +22,25 @@ class TestTimer(BaseTestCase):
         self.client._request = self.request_mock
         self.timer = Timer(self.client)
 
+    def test_sample_rate_configuration(self):
+        self.assertEqual(self.timer.rate, 1)
+        timer = Timer(self.client, 0.3)
+        self.assertEqual(timer.rate, 0.3)
+        with self.assertRaises(AssertionError):
+            timer.rate = "not a number"
+        with self.assertRaises(AssertionError):
+            timer.rate = 2
+        with self.assertRaises(AssertionError):
+            timer.rate = -0.3
+
+    def test_get_set_client(self):
+        self.assertEqual(self.timer.client, self.client)
+        client = Client('127.0.0.2')
+        timer = Timer(client)
+        self.assertEqual(timer.client, client)
+        timer.client = self.client
+        self.assertEqual(timer.client, self.client)
+
     def test_since(self):
         start_time = time()
         sleep(0.01)
@@ -33,6 +52,11 @@ class TestTimer(BaseTestCase):
         self.request_mock.reset_mock()
         self.assertEqual(self.timer.since("event", start_time, rate=0), self.timer)
         self.assertEqual(self.request_mock.call_count, 0)
+
+        with self.assertRaises(AssertionError):
+            self.timer.since("event", start_time, rate=-0.2)
+        with self.assertRaises(AssertionError):
+            self.timer.since("event", start_time, rate=1.01)
 
     def test_time_callable(self):
         self.assertRaises(AssertionError, self.timer.time_callable, "event", "I'm not callable")
@@ -67,6 +91,11 @@ class TestTimer(BaseTestCase):
         self.assertRegex(request, "with_args:[1-9]\d{0,3}\|ms")
         self.assertEqual(args_passed, [("arg1", "arg2"), dict(named_arg="named_value")])
 
+        with self.assertRaises(AssertionError):
+            self.timer.time_callable("event", self.wait_a_while, rate=-0.2)
+        with self.assertRaises(AssertionError):
+            self.timer.time_callable("event", self.wait_a_while, rate=1.01)
+
     def test_timer_function_decorator(self):
         nap_calls = []
 
@@ -95,7 +124,58 @@ class TestTimer(BaseTestCase):
         self.assertEqual(nap_low_rate_calls, ["low rate called"])
         self.assertEqual(self.request_mock.call_count, 0)
 
+        with self.assertRaises(AssertionError):
+            @self.timer.wrap("invalid_rate", -0.1)
+            def wont_be_called():
+                pass
+
     def wait_a_while(self):
         sleep(0.01)
         return "waited"
 
+
+class TestStopWatch(BaseTestCase):
+    def setUp(self):
+        self.client = Client('127.0.0.1')
+        self.request_mock = mock.MagicMock()
+        self.client._request = self.request_mock
+        self.metric_name = "timed_event"
+        self.stop_watch = StopWatch(self.client, self.metric_name)
+
+    def test_name(self):
+        stop_watch = StopWatch(self.client, "new_watch")
+        self.assertEqual(stop_watch.name, "new_watch")
+        stop_watch.name = self.metric_name
+        self.assertEqual(stop_watch.name, self.metric_name)
+
+    def test_sample_rate_configuration(self):
+        self.assertEqual(self.stop_watch.rate, 1)
+        stop_watch = StopWatch(self.client, "new_watch", rate=0.3)
+        self.assertEqual(stop_watch.rate, 0.3)
+        with self.assertRaises(AssertionError):
+            stop_watch.rate = "not a number"
+        with self.assertRaises(AssertionError):
+            stop_watch.rate = 2
+        with self.assertRaises(AssertionError):
+            stop_watch.rate = -0.3
+
+    def test_get_set_client(self):
+        self.assertEqual(self.stop_watch.client, self.client)
+        client = Client('127.0.0.2')
+        stop_watch = StopWatch(client, "new_watch")
+        self.assertEqual(stop_watch.client, client)
+        stop_watch.client = self.client
+        self.assertEqual(stop_watch.client, self.client)
+
+    def test_send(self):
+        sleep(0.01)
+        self.stop_watch.send()
+        self.assertEqual(self.request_mock.call_count, 1)
+        request_args = self.request_mock.call_args[0]
+        self.assertEqual(len(request_args), 1)
+        request = request_args[0]
+        self.assertRegex(request, "timed_event:[1-9]\d{0,3}\|ms")
+
+        self.request_mock.reset_mock()
+        self.stop_watch.send(rate=0)
+        self.assertEqual(self.request_mock.call_count, 0)
